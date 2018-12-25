66bd9e089aebcd69eae5a367bd0fd2dce469daaa
Mgazul <mgazul@163.com>
2018-12-25 20:44:03 +0800

    改善聊天格式

--- src/main/java/cn/mgazul/pfess/chat/ChatFormatListener.java
+++ src/main/java/cn/mgazul/pfess/chat/ChatFormatListener.java
@@ -1,6 +1,7 @@
 package cn.mgazul.pfess.chat;
 
 import cn.mgazul.pfess.PFessPapiHook;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,10 +12,8 @@ public class ChatFormatListener implements Listener{
 	  @EventHandler
 	  public void onChat(AsyncPlayerChatEvent e){
 	    Player p = e.getPlayer();
-	    if(e.getMessage() == null) {
-	    	return;
-	    }
-		e.setFormat(PFessPapiHook.replacepapi(p,"&f[%pfw_worldname%&f]%pfess_tags%&f<"+p.getName()+"&f>&7 ")+ e.getMessage());
+	    e.setCancelled(true);
+	    Bukkit.getServer().broadcastMessage(PFessPapiHook.replacepapi(p,"&f[%pfw_worldname%&f]%pfess_tags%&f<"+p.getName()+"&f>&7 ")+ e.getMessage());
 	  }
 	  
 }
